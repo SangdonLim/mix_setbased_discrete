24c5d9dd35b9822de2b531063e16b1168d2dc88f
sangdonlim <48362733+SangdonLim@users.noreply.github.com>
2023-11-17 18:42:54 -0600

    update filepaths

--- results/hash.R
+++ results/hash.R
@@ -1,11 +1,11 @@
 # result data is very large in size (~ 90 GB total)
 # because of this only hashes of result files are stored in the repo
 
-fp <- "~/Box Sync/Behaviormetrika_Special_Issue/Article_2_PassageCAT/Results"
+fp <- "results"
 
 # Study 1 data (3200 files, ~ 40 GB)
 
-new_snapshot <- fileSnapshot(file.path(fp, "Study1"), md5sum = TRUE, file.info = FALSE)
+new_snapshot <- fileSnapshot(file.path(fp, "study1"), md5sum = TRUE, file.info = FALSE)
 fn <- "results/study1_hash.rds"
 if (!file.exists(fn)) {
   saveRDS(new_snapshot, fn)
@@ -17,7 +17,7 @@ changedFiles(before = stored_snapshot, after = new_snapshot, check.file.info = N
 
 # Study 2 data (3600 files, ~ 50 GB)
 
-new_snapshot <- fileSnapshot(file.path(fp, "Study2"), md5sum = TRUE, file.info = FALSE)
+new_snapshot <- fileSnapshot(file.path(fp, "study2"), md5sum = TRUE, file.info = FALSE)
 fn <- "results/study2_hash.rds"
 if (!file.exists(fn)) {
   saveRDS(new_snapshot, fn)
